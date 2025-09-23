1dbd85c0539aa46d22f8771306be93c262fd2281
aeoncl <c.lopez.dev@protonmail.com>
2025-09-23 23:24:46 +0200

    proxy redirection working

--- src/network.rs
+++ src/network.rs
@@ -2,17 +2,23 @@ use std::net::Ipv4Addr;
 use std::sync::{Arc, Mutex};
 use netaddr2::{Mask, Netv4Addr};
 use netwatcher::WatchHandle;
+use tokio::sync::watch::Receiver;
 
 #[derive(Clone)]
 pub(crate) struct NetworkWatchHandle {
     network_type: Arc<Mutex<NetworkType>>,
+    notification_receiver: Receiver<NetworkType>,
     handle: Arc<Mutex<WatchHandle>>,
 }
 
 impl NetworkWatchHandle {
 
     pub fn network_type(&self) -> NetworkType {
-        self.network_type.lock().unwrap().clone()
+        self.notification_receiver.clone().borrow_and_update().clone()
+    }
+
+    pub fn subscribe(&self) -> Receiver<NetworkType> {
+        self.notification_receiver.clone()
     }
 
 }
@@ -20,6 +26,8 @@ impl NetworkWatchHandle {
 pub(crate) fn watch_networks(subnets: Vec<Netv4Addr>) -> NetworkWatchHandle {
 
     let network_type = Arc::new(Mutex::new(NetworkType::Direct));
+    let (notification_sender, notification_receiver) = tokio::sync::watch::channel::<NetworkType>(NetworkType::Direct);
+
 
     let cloned_network_type = network_type.clone();
     let handle = netwatcher::watch_interfaces(move |update| {
@@ -33,20 +41,36 @@ pub(crate) fn watch_networks(subnets: Vec<Netv4Addr>) -> NetworkWatchHandle {
             })
         });
 
+        
+        
+        let needs_updates = {
+            let mut network = cloned_network_type.lock().unwrap();
+            
+            if is_in_subnet && !matches!(*network, NetworkType::Proxied) {
+                *network = NetworkType::Proxied;
+                true
+            } else if !is_in_subnet && !matches!(*network, NetworkType::Direct){
+                *network = NetworkType::Direct;
+                true
+            } else {
+                false 
+            }
+        };
 
-        let mut network = cloned_network_type.lock().unwrap();
-
-        if is_in_subnet {
-            *network = NetworkType::Proxied;
-        } else {
-            *network = NetworkType::Direct;
+        if needs_updates {
+            if is_in_subnet {
+                notification_sender.send(NetworkType::Proxied).unwrap();
+            } else {
+                notification_sender.send(NetworkType::Direct).unwrap();
+            }
         }
-
-
+        
+      
     }).unwrap();
 
     NetworkWatchHandle {
         network_type,
+        notification_receiver,
         handle: Arc::new(Mutex::new(handle)),
     }
 }
